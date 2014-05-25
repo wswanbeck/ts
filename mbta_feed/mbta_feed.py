267a537148c4073c62a3e0c2596fab4908089eea
wswanbeck <wswanbeck@silversky.com>
2014-05-25 12:55:55 -0400

    read feeds into memory

--- mbta_feed/mbta_feed.py
+++ mbta_feed/mbta_feed.py
@@ -1,3 +1,9 @@
+import urllib2
+import json
+import time
+
+
+
 class MbtaFeed:
 
     feeds = ['http://developer.mbta.com/lib/RTCR/RailLine_9.json', # 9. Fitchburg
@@ -6,5 +12,37 @@ class MbtaFeed:
              'http://developer.mbta.com/lib/RTCR/RailLine_12.json', #12. Newburyport/Rockport
              ]
 
+    def __init__(self):
+        # self.vehicles[trip] = [time added, vehicle]
+        self.vehicles = dict()
+
     def read_feeds(self):
+        for feed in MbtaFeed.feeds:
+            self.read_feed(feed)
+
+    def read_feed(self, feed):
+        req = urllib2.Request(feed)
+        req.add_header('Content-Type', 'application/json')
+        response = urllib2.urlopen(req)
+        # let's hope that response.msg == 'OK' and response.code==200
+        data = None
+        if response.code == 200:
+            data =  json.load(response)
+
+        for message in data['Messages']:
+            stop = message['Stop']
+            dest = message['Destination']
+            vehicle = message['Vehicle']
+            trip = message['Trip']
+            speed = int(message['Speed'])
+            flag = message['Flag']
+
+            # vehicle is probably right if flag it not 'Pre' or 'Sch'
+            if flag.lower() not in ['pre', 'sch']:
+                self.save_vehicle(vehicle, trip)
+                break
         pass
+
+    def save_vehicle(self, vehicle, trip):
+        self.vehicles[trip] = [time.time(), vehicle]
+
